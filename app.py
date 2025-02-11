11c4f70d2bf1a21d660bebb97d46d0b647230375
Siddharth11Roy <155230297+Siddharth11Roy@users.noreply.github.com>
2025-02-11 10:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ col1, col2 = st.columns([2,3])
 
 # Streamlit UI
 st.markdown('<h1 class="title">📄 PDF to Speech Converter 🔊</h1>', unsafe_allow_html=True)
-st.title("📄 PDF to Speech Converter 🔊")
+st.title("📄Your personalised AI TOOL 🔊")
 # with col2:
 st_lottie(load_lottieurl("https://lottie.host/55a1cac3-de09-453c-874b-50907e9195e2/tF3TSNDTga.json"), width=1200, height=200)
 
